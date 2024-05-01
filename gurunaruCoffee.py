b4f8f0eee69fa347f0a9076d5da3e6bdb262f26c
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-05-01 17:42:31 +0900

    Update gurunaruCoffee.py

--- gurunaruCoffee.py
+++ gurunaruCoffee.py
@@ -1,30 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
-import time
 import json
+import datetime
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
 folder_path = "gurunaru"
 filename = f"{folder_path}/menugurunaru_{current_date}.json"
 
-# 웹드라이브 설치
-options = ChromeOptions()
-service = ChromeService(executable_path=ChromeDriverManager().install())
-browser = webdriver.Chrome(service=service, options=options)
-browser.get("https://www.coffine.co.kr/front/menu/coffee_list.php#contents")
-
-# 페이지가 완전히 로드될 때까지 대기
-WebDriverWait(browser, 10).until(
-    EC.presence_of_element_located((By.CLASS_NAME, "pro_list"))
-)
+# 웹 페이지로부터 데이터 요청 및 수신
+response = requests.get("https://www.coffine.co.kr/front/menu/coffee_list.php#contents")
+soup = BeautifulSoup(response.text, "lxml")
 
 # 업데이트된 페이지 소스를 변수에 저장
 html_source_updated = browser.page_source
