df815d969d44b16f501e5e9ec03ce6d5641e95a3
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-05-01 17:46:43 +0900

    Update gurunaruCoffee.py

--- gurunaruCoffee.py
+++ gurunaruCoffee.py
@@ -1,7 +1,14 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
+from datetime import datetime
+import time
 import json
-import datetime
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
