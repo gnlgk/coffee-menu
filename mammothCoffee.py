26365875b9f633c9a3953a29167f669d3f6da968
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-05-01 17:00:33 +0900

    Update mammothCoffee.py

--- mammothCoffee.py
+++ mammothCoffee.py
@@ -13,7 +13,7 @@ import json
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
 folder_path = "mammoth"
-filename = f"{folder_path}/menu-mammoth_{current_date}.json"
+filename = f"{folder_path}/menumammoth_{current_date}.json"
 
 # 웹드라이브 설치
 options = ChromeOptions()
