1b3ac96ee299cb0b87e3c584db00bc3969e871cb
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-05-01 17:00:10 +0900

    Update hollysCoffee.py

--- hollysCoffee.py
+++ hollysCoffee.py
@@ -13,7 +13,7 @@ import json
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
 folder_path = "hollys"
-filename = f"{folder_path}/menu-hollys_{current_date}.json"
+filename = f"{folder_path}/menuhollys_{current_date}.json"
 
 # 웹드라이브 설치
 options = ChromeOptions()
