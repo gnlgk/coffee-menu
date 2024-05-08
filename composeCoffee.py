dd5606405ccd480a72d8624bc1293400d1a1180c
gnlgk <161431748+gnlgk@users.noreply.github.com>
2024-05-08 16:51:51 +0900

    Update composeCoffee.py

--- composeCoffee.py
+++ composeCoffee.py
@@ -1,8 +1,14 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from datetime import datetime
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+import time
 import json
 import os
 
